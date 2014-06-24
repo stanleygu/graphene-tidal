2ee27bf59b6230555d5c2458839f6e9ebef99dd1
Stanley Gu <stanleygu@gmail.com>
2014-06-23 18:33:50 -0700

    Refactor layout logic into layout controller

--- app/scripts/controllers/tidalLayoutCtrl.js
+++ app/scripts/controllers/tidalLayoutCtrl.js
@@ -11,6 +11,123 @@ angular.module('grapheneTidalApp')
       normal: 1
     };
 
+    var runLayout = function(data) {
+      $scope.edges = _.map(data.edges, function(edge) {
+        return {
+          source: _.find(data.nodes, function(n) {
+            return n.id === edge.nodes[0];
+          }),
+          target: _.find(data.nodes, function(n) {
+            return n.id === edge.nodes[1];
+          }),
+          type: edge.type
+        };
+      });
+      $scope.nodes = data.nodes;
+
+      var sections = data.timeSlots;
+
+      var orderedKeys = _.keys(sections).sort(function(a, b) {
+        var reA = /[^a-zA-Z]/g;
+        var reN = /[^0-9]/g;
+        var aA = a.replace(reA, '');
+        var bA = b.replace(reA, '');
+        if (aA === bA) {
+          var aN = parseInt(a.replace(reN, ''), 10);
+          var bN = parseInt(b.replace(reN, ''), 10);
+          return aN === bN ? 0 : aN > bN ? 1 : -1;
+        } else {
+          return aA > bA ? 1 : -1;
+        }
+      });
+
+      var sizes = {};
+      sizes.max = _.max(data.nodes, function(n) {
+        return n.size;
+      });
+      sizes.min = _.min(data.nodes, function(n) {
+        return n.size;
+      });
+      $scope.groups = [];
+      var count = 0;
+      var layoutCompleteCount = 0;
+      _.each(orderedKeys, function(key) {
+        var sect = sections[key];
+        var nodes = _.filter(data.nodes, function(n) {
+          return _.contains(sect, n.id);
+        });
+        _.each(nodes, function(n) {
+          n.group = count;
+          n.scaleFactor = (n.size - sizes.min.size) / (sizes.max.size - sizes.min.size);
+          n.width = $scope.imports.nodeSize.min.width +
+            (($scope.imports.nodeSize.max.width - $scope.imports.nodeSize.min.width) *
+            n.scaleFactor);
+          n.height = $scope.imports.nodeSize.min.height +
+            (($scope.imports.nodeSize.max.height - $scope.imports.nodeSize.min.height) *
+            n.scaleFactor);
+        });
+
+        var links = _.filter($scope.edges, function(l) {
+          return _.contains(sect, l.source.id) && _.contains(sect, l.target.id);
+        });
+
+        var force = d3.layout.force()
+          .charge($scope.imports.charge || -700)
+          .linkDistance($scope.imports.linkDistance || 40)
+          .gravity($scope.imports.gravity || 0.1)
+          .size([$scope.imports.subgraph.width || 800, $scope.imports.subgraph.height || 800]);
+        _.each(nodes, function(n) {
+          n.force = force;
+        });
+        var ran = false;
+        force
+          .nodes(nodes)
+          .on('tick', function() {
+            if ($scope.imports.subgraph.height && $scope.imports.subgraph.width) {
+              _.each(nodes, function(n) {
+                n.x = Math.max(n.width, Math.min($scope.imports.subgraph.width -
+                  n.width, n.x));
+                n.y = Math.max(n.height, Math.min($scope.imports.subgraph.height -
+                  n.height, n.y));
+              });
+              if (!ran) {
+                $scope.$digest();
+                ran = true;
+              }
+            }
+            var thres = $scope.layoutStopThreshold || 0.01;
+            if (force.alpha() <= thres) {
+              force.stop();
+              $scope.$digest();
+              if ($scope.layoutComplete) {
+                layoutCompleteCount += 1;
+                if (layoutCompleteCount === $scope.groups.length) {
+                  $scope.callWindowFunction($scope.layoutComplete);
+                }
+              }
+            }
+          })
+          .start();
+        $scope.groups.push({
+          nodes: nodes,
+          links: links,
+          name: key
+        });
+        count += 1;
+      });
+
+      $scope.lookup = {};
+      $scope.lookup.node = _.indexBy(data.nodes, 'name');
+
+      _.each($scope.edges, function(edge) {
+        edge.source.from = edge.source.from || [];
+        edge.source.from.push(edge);
+
+        edge.target.to = edge.target.to || [];
+        edge.target.to.push(edge);
+      });
+    };
+
 
     // COMPUTED LINK PROPERTY
     var updateLinkPosition = function(link) {
@@ -43,10 +160,9 @@ angular.module('grapheneTidalApp')
       link.y2 = sourceToTarget.y;
     };
 
-    $scope.$watchCollection('imports.edges', function(val) {
+    $scope.$watchCollection('edges', function(val) {
       if (val) {
-        $scope.links = $scope.imports.edges;
-        _.each($scope.links, function(l) {
+        _.each($scope.edges, function(l) {
           $scope.$watch(function() {
             return l.source.x + l.source.y + l.target.x + l.target.y;
           }, function() {
@@ -67,10 +183,10 @@ angular.module('grapheneTidalApp')
 
     $scope.mouseoverNode = function(node) {
       var OPAC = $scope.OPACITY;
-      _.each($scope.imports.edges, function(e) {
+      _.each($scope.edges, function(e) {
         e.opacity = OPAC.unfocused;
       });
-      _.each($scope.imports.nodes, function(n) {
+      _.each($scope.nodes, function(n) {
         n.opacity = OPAC.unfocused;
       });
       node.opacity = OPAC.focused;
@@ -86,12 +202,18 @@ angular.module('grapheneTidalApp')
 
     $scope.mouseleaveNode = function() {
       var OPAC = $scope.OPACITY;
-      _.each($scope.imports.nodes, function(n) {
+      _.each($scope.nodes, function(n) {
         n.opacity = OPAC.normal;
       });
-      _.each($scope.imports.edges, function(e) {
+      _.each($scope.edges, function(e) {
         e.opacity = OPAC.normal;
       });
     };
+
+    $scope.$watch('imports.data', function(newVal) {
+      if (newVal) {
+        runLayout(newVal);
+      }
+    });
 
   });
